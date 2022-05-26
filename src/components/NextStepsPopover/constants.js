4e7ef73a8da28e8b6c56158d821c6dacf6cfa789
Karson Kalt <kars.kalt@gmail.com>
2022-05-26 19:10:40 -0400

    add new todos

--- src/components/NextStepsPopover/constants.js
+++ src/components/NextStepsPopover/constants.js
@@ -1,5 +1,9 @@
 export default [
-  ' Hoist search text state to App, render Tweets component from App instead of as a child of Search',
-  'Create an "add post" form and show created posts',
-  'Another suggestion?',
+  'Make "DUMB" components that are for presentation only e.g. a Button component',
+  'We need to make an icon button component that manages the hover effects, css, sizing, etc',
+  "Build a login, store that user that's logged in",
+  'Display the logged in user',
+  'Likes should be managed by the backend and clicking should increase that like',
+  'We could do something with the promoted tweets to highlight them',
+  'Users can have profiles and we can see their tweets',
 ];
